61623556392221542e62fbb0c6cd094ccf210726
jf-guillou <jean.f.guillou@gmail.com>
2016-10-05 16:31:50 +0200

    Prefill some content type models

---
+++ models/types/Agenda.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models\types;
+
+/**
+ * This is the model class for content uploads.
+ */
+class Agenda extends RSS
+{
+}
---
+++ models/types/RSS.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\models\types;
+
+use app\models\Content;
+
+/**
+ * This is the model class for content uploads.
+ */
+class RSS extends Content
+{
+}
