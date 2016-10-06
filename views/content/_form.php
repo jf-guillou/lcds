8a8601ca6e586cffcdc692cf048469de01cf2725
jf-guillou <jean.f.guillou@gmail.com>
2016-10-06 17:30:02 +0200

    Fixed Datepicker time format

--- views/content/_form.php
+++ views/content/_form.php
@@ -34,13 +34,13 @@ use kartik\datetime\DateTimePicker;
         </div>
         <div class="col-lg-5">
             <?= $form->field($model, 'start_ts')->widget(DateTimePicker::className(), [
-                    'pluginOptions' => ['format' => 'yyyy-mm-dd HH:mm:ss'],
+                    'pluginOptions' => ['format' => 'yyyy-mm-dd hh:mm:ss'],
                 ]) ?>
         </div>
 
         <div class="col-lg-5">
             <?= $form->field($model, 'end_ts')->widget(DateTimePicker::className(), [
-                    'pluginOptions' => ['format' => 'yyyy-mm-dd HH:mm:ss'],
+                    'pluginOptions' => ['format' => 'yyyy-mm-dd hh:mm:ss'],
                 ]) ?>
         </div>
     </div>
--- views/content/type/url.php
+++ views/content/type/url.php
@@ -44,13 +44,13 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
             <div class="col-lg-5">
                 <?= $form->field($model, 'start_ts')->widget(DateTimePicker::className(), [
-                        'pluginOptions' => ['format' => 'yyyy-mm-dd HH:mm:ss'],
+                        'pluginOptions' => ['format' => 'yyyy-mm-dd hh:mm:ss'],
                     ]) ?>
             </div>
 
             <div class="col-lg-5">
                 <?= $form->field($model, 'end_ts')->widget(DateTimePicker::className(), [
-                        'pluginOptions' => ['format' => 'yyyy-mm-dd HH:mm:ss'],
+                        'pluginOptions' => ['format' => 'yyyy-mm-dd hh:mm:ss'],
                     ]) ?>
             </div>
         </div>
