1cd43cd1fcbd745e18347f9d5edb48dc755d503b
jf-guillou <jean.f.guillou@gmail.com>
2016-10-07 17:16:43 +0200

    Started agenda content type widget

--- models/types/Agenda.php
+++ models/types/Agenda.php
@@ -2,6 +2,8 @@
 
 namespace app\models\types;
 
+use ICal\ICal;
+
 /**
  * This is the model class for content uploads.
  */
@@ -11,4 +13,48 @@ class Agenda extends RSS
     public static $typeDescription = 'Display an agenda from an RSS feed.';
     public static $html = '<div class="agenda" data-url="%data%"></div>';
     public static $usable = true;
+
+    protected static function downloadFeed($url)
+    {
+        if (\Yii::$app->params['proxy']) {
+            $ctx = [
+                'http' => [
+                    'proxy' => 'tcp://vdebian:8080',
+                    'request_fulluri' => true,
+                ],
+            ];
+
+            return file_get_contents($url, false, stream_context_create($ctx));
+        } else {
+            return file_get_contents($url);
+        }
+    }
+
+    public static function processData($data)
+    {
+        $content = self::downloadFeed($data);
+
+        $ical = new ICal();
+        $ical->defaultWeekStart = 'MO';
+        $ical->initString($content);
+
+        $events = $ical->eventsFromRange('last monday', 'next friday');
+        if (!count($events)) {
+            return;
+        }
+
+        var_dump($events[0]);
+        exit;
+
+        foreach ($events as $e) {
+            $e['summary']; // name
+            $e['dtstart'];
+            $e['dtend'];
+            $e['description'];
+            $e['location'];
+        }
+        exit;
+
+        return $data;
+    }
 }
