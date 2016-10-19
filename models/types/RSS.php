8526e6b3e3a44ad25ca9cbbe7dee0c05ac3de7eb
jf-guillou <jean.f.guillou@gmail.com>
2016-10-19 18:10:29 +0200

    Create template for RSS, but no design since it'll probably be too specific

--- models/types/RSS.php
+++ models/types/RSS.php
@@ -11,8 +11,47 @@ class RSS extends Content
 {
     public static $typeName = 'RSS';
     public static $typeDescription = 'Display an RSS feed inline.';
-    public static $html = '<div class="rss" data-url="%data%"></div>';
+    public static $html = '<div class="rss">%data%</div>';
     public static $input = 'url';
-    public static $output = null;
+    public static $output = 'text';
     public static $usable = false;
+    public static $preview = null;
+
+    /**
+     * Downloads feed from URL through proxy if necessary.
+     *
+     * @param string $url
+     *
+     * @return string feed content
+     */
+    protected static function downloadFeed($url)
+    {
+        if (\Yii::$app->params['proxy']) {
+            $ctx = [
+                'http' => [
+                    'proxy' => 'tcp://vdebian:8080',
+                    'request_fulluri' => true,
+                ],
+            ];
+
+            return file_get_contents(urldecode($url), false, stream_context_create($ctx));
+        } else {
+            return file_get_contents(urldecode($url));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function processData($data)
+    {
+        // Fetch content from cache if possible
+        $content = self::fromCache($data);
+        if (!$content) {
+            $content = self::downloadFeed($data);
+            self::toCache($data, $content, self::BASE_CACHE_TIME);
+        }
+
+        return nl2br(Html::encode($content));
+    }
 }
