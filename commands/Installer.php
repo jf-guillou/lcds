acd5afb179b7431c6682ac776b55479c7877ee47
jf-guillou <jean.f.guillou@gmail.com>
2016-10-31 14:48:19 +0100

    Tweaked composer install scripts to copy configuration
    Also change db configuration on the fly for Travis

---
+++ commands/Installer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\commands;
+
+class Installer
+{
+    /**
+     * Post composer install event script.
+     *
+     * @param mixed $event composer event
+     */
+    public static function postInstall($event)
+    {
+        $params = $event->getComposer()->getPackage()->getExtra();
+        if (isset($params[__METHOD__]) && is_array($params[__METHOD__])) {
+            foreach ($params[__METHOD__] as $method => $args) {
+                call_user_func_array([__CLASS__, $method], (array) $args);
+            }
+        }
+    }
+
+    /**
+     * Copy configuration files on composer install script end.
+     *
+     * @param array $paths configuration files
+     */
+    public static function copyConfiguration(array $paths)
+    {
+        foreach ($paths as $from => $to) {
+            echo 'mv '.$from.' '.$to.': ';
+            if (is_dir($to) || is_file($to)) {
+                echo 'destination file exists.'.PHP_EOL;
+            } elseif (is_dir($from) || is_file($from)) {
+                if (rename($from, $to)) {
+                    echo 'done.'.PHP_EOL;
+                } else {
+                    echo 'error while moving file.'.PHP_EOL;
+                }
+            } else {
+                echo 'file not found.'.PHP_EOL;
+            }
+        }
+    }
+}
