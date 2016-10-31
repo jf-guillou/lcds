86c0eb4912c14cf8c7a57016e19019408a011b8f
jf-guillou <jean.f.guillou@gmail.com>
2016-10-31 17:43:17 +0100

    Still don't understand how codeception should work

--- config/test.php
+++ config/test.php
@@ -5,13 +5,13 @@ $params = require __DIR__.'/params.php';
 $config = [
     'id' => 'lcds',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'defaultRoute' => 'main/index',
     'sourceLanguage' => 'en-US',
     'language' => $params['language'],
     'components' => [
         'request' => [
-            'cookieValidationKey' => $params['cookieValidationKey'],
+            'cookieValidationKey' => 'test',
+            'enableCsrfValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -35,16 +35,6 @@ $config = [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'logFile' => '@runtime/logs/errors.log',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
         'i18n' => [
             'translations' => [
                 'app' => [
@@ -67,20 +57,5 @@ $config = [
     ],
     'params' => $params,
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        'allowedIPs' => $params['debugAllowedIPs'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        'allowedIPs' => $params['debugAllowedIPs'],
-    ];
-}
 
 return $config;
