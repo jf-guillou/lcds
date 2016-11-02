c37779964a76f301b1f8a8542783575165c7e30f
jf-guillou <jean.f.guillou@gmail.com>
2016-11-02 14:14:32 +0100

    Hyphens in database name are not legal :(
    Added support for PHP 5.4 & 5.5

--- config/db.test.php
+++ config/db.test.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=lcds-test',
+    'dsn' => 'mysql:host=localhost;dbname=lcds_test',
     'username' => 'travis',
     'password' => '',
     'charset' => 'utf8',
