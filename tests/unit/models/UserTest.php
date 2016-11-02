2f80edcc5340c46957ff3a1200e4012ccc4cd883
jf-guillou <jean.f.guillou@gmail.com>
2016-11-02 18:05:07 +0100

    Some more User unit tests

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -10,6 +10,7 @@ class UserTest extends \Codeception\Test\Unit
     {
         expect_that($user = User::findIdentity('admin'));
         expect($user->username)->equals('admin');
+        expect_not($user->fromLdap);
 
         expect_not(User::findIdentity('non-existing-user'));
     }
@@ -23,5 +24,33 @@ class UserTest extends \Codeception\Test\Unit
 
         expect_that($user->validatePassword('admin'));
         expect_not($user->validatePassword('123456'));
+    }
+
+    public function testCreateUser()
+    {
+        $user = User::create('test', 'test');
+        expect_that($user);
+
+        $user = User::findIdentity('test');
+        expect_that($user);
+        expect_that($user->authkey);
+        expect_that($user->access_token);
+        expect_not($user->fromLdap);
+        expect_that($user->validatePassword('test'));
+    }
+
+    public function testAuthenticate()
+    {
+        $user = User::findIdentity('admin');
+
+        expect_that($user->authenticate('admin'));
+    }
+
+    public function testFindInLdap()
+    {
+        // We cannot test this method, only proper error return
+        $user = User::findInLdap('non-existing-ldap-user');
+
+        expect_not($user);
     }
 }
