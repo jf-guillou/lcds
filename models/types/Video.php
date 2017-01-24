d61c7d9df8d3e37f7bfd4b5b4272decc0e3551fe
jf-guillou <jean.f.guillou@gmail.com>
2017-01-24 18:45:40 +0100

    Force stretch content only for iframes

--- models/types/Video.php
+++ models/types/Video.php
@@ -13,7 +13,7 @@ class Video extends Media
     const TYPE_PATH = 'videos/';
 
     public $html = '<iframe src="%data%" />';
-    public $css = '%field% > * { height: 100%; width: 100%; }';
+    public $css = '%field% > iframe { height: 100%; width: 100%; }';
     public $appendParams = '_win=%x1%,%y1%,%x2%,%y2%;_aspect-mode=letterbox';
     public $usable = true;
     public $input = 'url';
