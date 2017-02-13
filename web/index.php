a1917515ac71f125d0a870c5ad9b53e944635f0d
jf-guillou <jean.f.guillou@gmail.com>
2017-02-13 11:15:13 +0100

    Try to please scrutinizer with debug statements

--- web/index.php
+++ web/index.php
@@ -1,7 +1,11 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') ?: false);
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');
+if (!defined('YII_DEBUG')) {
+    define('YII_DEBUG', getenv('YII_DEBUG') ?: false);
+}
+if (!defined('YII_ENV')) {
+    define('YII_ENV', getenv('YII_ENV') ?: 'prod');
+}
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
