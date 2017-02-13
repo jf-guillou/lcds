d779660b1a60638fa3e2dcbc5868787c4b57910c
jf-guillou <jean.f.guillou@gmail.com>
2017-02-13 09:27:54 +0100

    Another try at debug fixes

--- web/index.php
+++ web/index.php
@@ -1,8 +1,7 @@
 <?php
 
-// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') || false);
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') || 'prod');
-defined('YII_DEBUG') or define('YII_DEBUG', YII_ENV == 'dev');
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
