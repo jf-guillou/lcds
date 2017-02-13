0ee1437977008af68fc754d8d6a539c39e003d26
jf-guillou <jean.f.guillou@gmail.com>
2017-02-13 09:39:34 +0100

    How can I get this debug thing so wrong ?

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') || false);
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') || 'prod');
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') ?: false);
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../vendor/yiisoft/yii2/Yii.php';
